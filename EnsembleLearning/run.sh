8252467b3a16d2b0756b39dbd1471a31a8a01009
Jiaxuan-Peng <78872967+Jiaxuan-Peng@users.noreply.github.com>
2022-10-22 07:27:17 +0200

    Create run.sh

---
+++ EnsembleLearning/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/adaboost.py
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/bagging.py
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/random_forest.py
+
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/credit_adaboost.py
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/credit_bagging.py
+python3 /home/u1413911/Downloads/Machine-Learning-main/EnsembleLearning/credit_rf.py
