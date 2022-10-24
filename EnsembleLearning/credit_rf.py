93abc6098f9d7e9ccd84d352a14dae641e6d1840
Jiaxuan-Peng <78872967+Jiaxuan-Peng@users.noreply.github.com>
2022-10-24 09:47:39 +0200

    Update credit_rf.py

--- EnsembleLearning/credit_rf.py
+++ EnsembleLearning/credit_rf.py
@@ -1,261 +1,80 @@
 import math
 import copy
 import numpy as np
-import statistics
 import matplotlib.pyplot as plt
-import random
 import pandas as pd
+from DT import Tree, cal_gain,Node, pred
+import random
 
 random.seed(1)
 
-
-class Node:
-    def __init__(self, label):
-        self.label = label
-        self.children = dict()
-
-    def isLeaf(self):
-        return len(self.children) == 0
-
-
-def get_total(data):
-    total = 0.0
-
-    for row in data:
-        total += row['weights']
-
-    return total
-
-
-def set_subset(data, attribute, val):
-    sub_data = []
-
-    for row in data:
-        if row[attribute] == val:
-            sub_data.append(row)
-
-    return sub_data
-
-
-def set_label(row, dt):
-    new_dt = dt
-
-    while not new_dt.isLeaf():
-        curr_attr = new_dt.label
-        attr_val = row[curr_attr]
-        new_dt = new_dt.children[attr_val]
-
-    return new_dt.label
-
-
-def entropy(data):
-    if len(data) == 0:
-        return 0
-
-    counts = dict()
-
-    for row in data:
-        label = row['y']
-        if label not in counts:
-            counts[label] = 0.0
-
-        counts[label] += row['weights']
-
-    entropy = 0.0
-    total = get_total(data)
-    for (label, count) in counts.items():
-        p = count / total
-        entropy += -p * math.log2(p)
-
-    return entropy
-
-
-def gini_index(data):
-    if len(data) == 0:
-        return 0
-
-    counts = dict()
-
-    for row in data:
-        label = row['y']
-        if label not in counts:
-            counts[label] = 0.0
-        counts[label] += row['weights']
-
-    sq_sum = 0.0
-    total = get_total(data)
-    for (label, count) in counts.items():
-        p = count / total
-        sq_sum += p ** 2
-
-    return 1 - sq_sum
-
-
-def ME(data):
-    if len(data) == 0:
-        return 0
-
-    counts = dict()
-
-    for row in data:
-        label = row['y']
-        if label not in counts:
-            counts[label] = 0.0
-        counts[label] += row['weights']
-
-    max_p = 0.0
-    total = get_total(data)
-    for (label, count) in counts.items():
-        p = count / total
-        max_p = max(max_p, p)
-
-    return 1 - max_p
-
-
-def info_gain(data, gain_type, attribute, vals):
-    measure = None
-    gain = 0.0
-    if gain_type == 0:
-        measure = entropy(data)
-
-    elif gain_type == 1:
-        measure = gini_index(data)
-
-    elif gain_type == 2:
-        measure = ME(data)
-
-    for val in vals:
-        sub_set = set_subset(data, attribute, val)
-        total = get_total(data)
-        sub_total = get_total(sub_set)
-        p = sub_total / total
-        gain += p * measure
-        gain_x = measure - gain
-
-    return gain_x
-
-
-def select_feature(data, gain_type, attributes):
-    gain_x = dict()
-
-    for ln, lv in attributes.items():
-        gain = info_gain(data, gain_type, ln, lv)
-        gain_x[ln] = gain
-        max_attr = max(gain_x.keys(), key=lambda key: gain_x[key])
-
-    return max_attr
-
-
-def select_random_feat(attributes, size):
-    sub_attr = dict()
-    attr_copy = list(attributes.keys())
-
-    # select random features
-    while len(sub_attr) < size:
-        idx = random.randint(0, len(attr_copy) - 1)
-        attr = attr_copy[idx]
-        if attr not in sub_attr:
-            sub_attr[attr] = attributes[attr]
-    return sub_attr
-
-
-def majority_label(data):
-    counts = dict()
-    for row in data:
-        label = row['y']
-
-        if label not in counts:
-            counts[label] = 0.0
-
-        counts[label] += row['weights']
-
-    common_label = max(counts.keys(), key=lambda key: counts[key])
-
-    return common_label
-
-
-def ID3(data, gain_type, attributes, labels, max_depth, depth, size):
-    if (len(attributes) == 0) or depth == max_depth:
-        label = majority_label(data)
-
-        return Node(label)
-
+def ID3(dataset, gain, x_dic, labels, max_dep, size):
+    if (len(x_dic) == 0) or max_dep == 0:
+        c = {}
+        for i in dataset:
+            label = i['label']
+            if label not in c:
+                c[label] = 0
+            c[label] += i['w']
+        node = max(c.keys(), key=lambda key: c[key])
+        return Tree(node)
     if (len(labels) == 1):
-        label = labels.pop()
-
-        return Node(label)
-
-    # recursion
-    max_attr = select_feature(data, gain_type, attributes)
-    root = Node(max_attr)
-
-    # split into subsets
-    for v in attributes[max_attr]:
-        sub_set = set_subset(data, max_attr, v)
-
+        return Tree(labels.pop())
+    total_gain = {}
+    for ln, lv in x_dic.items():
+        total_gain_temp = cal_gain(dataset, gain, ln, lv)
+        total_gain[ln] = total_gain_temp
+        best_attr = max(total_gain.keys(), key=lambda key: total_gain[key])
+    root = Tree(best_attr)
+    for v in x_dic[best_attr]:
+        sub_set = []
+        for i in dataset:
+            if i[best_attr] == v:
+                sub_set.append(i)
         if len(sub_set) == 0:
-            label = majority_label(data)
-            root.children[v] = Node(label)
-
+            c = {}
+            for i in dataset:
+                label = i['label']
+                if label not in c:
+                    c[label] = 0
+                c[label] += i['w']
+            node = max(c.keys(), key=lambda key: c[key])
+            root.child[v] = Tree(node)
         else:
-
-            sub_attributes = copy.deepcopy(attributes)
-            sub_attributes.pop(max_attr)
-            sub_attributes = select_random_feat(sub_attributes, size)
-
-            # update subset labels set
+            sub_x_dic = copy.copy(x_dic)
+            sub_x_dic.pop(best_attr)
+            sub_attr = {}
+            dict_copy = list(sub_x_dic.keys())
+            while len(sub_attr) < size:
+                id = random.randint(0, len(dict_copy) - 1)
+                dict = dict_copy[id]
+                if dict not in sub_attr:
+                    sub_attr[dict] = sub_x_dic[dict]
             sub_labels = set()
-            for row in sub_set:
-                sub_label = row['y']
+            for i in sub_set:
+                sub_label = i['label']
                 if sub_labels not in sub_labels:
                     sub_labels.add(sub_label)
-
-            # recursion
-            root.children[v] = ID3(sub_set, gain_type, sub_attributes, sub_labels, max_depth, depth + 1, size)
-
+            root.child[v] = ID3(sub_set, gain, sub_attr, sub_labels, max_dep-1, size)
     return root
 
-
-def cls(data, gain_type, attributes, labels, max_depth, depth, T, size):
+def fit(dataset, gain, x_dic, labels, max_dep, T, size):
     DT = []
     for t in range(0, T):
-        random_data = [random.choice(data) for i in range(len(data))]
-        dt = ID3(data, gain_type, attributes, labels, max_depth, depth, size)
+        r_data = [random.choice(dataset) for i in range(len(dataset))]
+        dt = ID3(r_data, gain, x_dic, labels, max_dep, size)
         DT.append(dt)
-
     return DT
 
-
-def prediction(data, DT):
-    h_rate = 0
-
-    for row in data:
-        pred = 0.0
-
-        for dt in DT:
-            label = set_label(row, dt)
-            label = 1 if label == 'yes' else -1
-            pred += label
-
-        if row['y'] == 'yes' and pred > 0:
-            h_rate += 1
-        if row['y'] == 'no' and pred < 0:
-            h_rate += 1
-
-    return h_rate / len(data)
-
-
 if __name__ == '__main__':
-    columns = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14', 'X15', 'X16',
-               'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23", "y"]
+    x_col = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14', 'X15', 'X16',
+               'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23", "label"]
     labels = {"yes", "no"}
-    numeric_features = ['X1', 'X5', 'X12', 'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23"]
-    attributes = {'X1': [0, 1],'X2': [1, 2],'X3': [0, 1, 2, 3, 4, 5, 6],'X4': [0, 1, 2, 3],
-                  'X5': [0, 1],'X6': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X7': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-                  'X8': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X9': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-                  'X10': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X11': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-                  'X12': [0, 1],'X13': [0, 1],'X14': [0, 1],'X15': [0, 1],'X16': [0, 1],'X17': [0, 1],'X18': [0, 1],
-                  'X19': [0, 1],'X20': [0, 1],'X21': [0, 1],'X22': [0, 1],"X23": [0, 1], }
+    x_dic_numeric = ['X1', 'X5', 'X12', 'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23"]
+    x_dic = {'X1': [0, 1],'X2': [1, 2],'X3': [0, 1, 2, 3, 4, 5, 6],'X4': [0, 1, 2, 3],'X5': [0, 1],'X6': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+             'X7': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X8': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X9': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+             'X10': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X11': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X12': [0, 1],'X13': [0, 1],'X14': [0, 1],
+             'X15': [0, 1],'X16': [0, 1],'X17': [0, 1],'X18': [0, 1],'X19': [0, 1],'X20': [0, 1],'X21': [0, 1],'X22': [0, 1],"X23": [0, 1]}
 
 '''
 data = pd.read_csv('credit.csv', header=None)
@@ -264,77 +83,143 @@ test_data = data.iloc[24000:]
 train_data.to_csv('train.csv', header=None, index=False)
 test_data.to_csv('test.csv', header=None, index=False)
 '''
+train = []
+y_train = []
+test = []
+y_test = []
 
-# read training data
-train_data = []
-with open('D:/utah/courses/y1s1/ml/h2/credit/train.csv', 'r') as f:
+with open('train.csv', 'r') as f:
     for line in f:
-        example = dict()
-        terms = line.strip().split(',')
-        for i in range(len(terms)):
-            attrName = columns[i]
-            example[attrName] = terms[i]
+        x_val = {}
+        term = line.strip().split(',')
+        for i in range(len(term)):
+            x_val[x_col[i]] = term[i]
+        train.append(x_val)
+        y_train.append(term[-1])
 
-        train_data.append(example)
-# read test data
-test_data = []
-with open('D:/utah/courses/y1s1/ml/h2/credit/test.csv', 'r') as f:
+# use the median to convert the value
+traindf = pd.DataFrame(train)
+traindf = traindf.drop([0, 1])
+
+for i in x_dic_numeric:
+    traindf[i] = np.where(traindf[i].astype(int) <= (int(traindf[i].median())), 0, traindf[i])
+    traindf[i] = np.where(traindf[i].astype(int) > (int(traindf[i].median())), 1, traindf[i])
+train = traindf.to_dict('records')
+
+# for i in traindf:
+#    print(set(train[i].values()))
+
+with open('test.csv', 'r') as f:
     for line in f:
-        example = dict()
-        terms = line.strip().split(',')
-        for i in range(len(terms)):
-            attrName = columns[i]
-            example[attrName] = terms[i]
+        x_val = {}
+        term = line.strip().split(',')
+        for i in range(len(term)):
+            x_val[x_col[i]] = term[i]
+        test.append(x_val)
+        y_test.append(term[-1])
 
-        test_data.append(example)
-#convert numeric variable to binary variable
-medians = {'X1':0, 'X5':0, 'X12':0, 'X13':0, 'X14':0, 'X15':0, 'X16':0, 'X17':0, 'X18':0, 'X19':0, 'X20':0, 'X21':0, 'X22':0,"X23":0}
-for key in medians.keys():
-    vals = []
-    for row in train_data:
-        vals.append(float(row[key]))
-    medians[key] = statistics.median(vals)
-# if >median, 1 else 0
-for key, value in medians.items():
-    for row in train_data:
-        val = float(row[key])
-        row[key] = 1 if val > value else 0
+# use the median to convert the value
+testdf = pd.DataFrame(test)
+testdf = testdf.drop([0, 1])
 
-    for row in test_data:
-        val = float(row[key])
-        row[key] = 1 if val > value else 0
+for i in x_dic_numeric:
+    testdf[i] = np.where(testdf[i].astype(int) <= (int(testdf[i].median())), 0, testdf[i])
+    testdf[i] = np.where(testdf[i].astype(int) > (int(testdf[i].median())), 1, testdf[i])
+test = testdf.to_dict('records')
 
-# initialize the weight
-for row in train_data:
-    row['weights'] = 1 / len(train_data)
-
-for row in test_data:
-    row['weights'] = 1 / len(test_data)
-
-def update_weight(data):
-    update_w = 1 / len(data)
-    for row in data:
-        row['weights'] = update_w
-
-
-train_T = []
-test_T = []
+print("it's generating Errors vary with the number of random trees...")
+tr = []
+te = []
 for T in range(25):  # 500
-    trees = cls(train_data, 0, attributes, labels, 1, 0, T, 2)
-    h_train = prediction(train_data, trees)
-    h_test = prediction(test_data, trees)
-    h_x = 1 - h_train
-    h_y = 1 - h_test
-    train_T.append(h_x)
-    test_T.append(h_y)
+    trees = fit(train, 'EP', x_dic, labels, 1e+8, T, 2)
+    h_tr = pred(train, trees)
+    h_te = pred(test, trees)
+    h_x = 1 - h_tr
+    h_y = 1 - h_te
+    tr.append(h_x)
+    te.append(h_y)
+    update_w = 1 / float(len(train))
+    for i in train:
+        i['w'] = update_w
+
 x1 = [x for x in range(25)]  # 500
-y1 = train_T
-y2 = test_T
 fig, ax = plt.subplots()
-ax.plot(x1, y1, label='train error', c='black')
-ax.plot(x1, y2, label='test error', c='red')
+ax.plot(x1, tr, label='train error', c='red')
+ax.plot(x1, te, label='test error', c='green')
 ax.legend()
-#ax.set_title("Errors vary with the number of random trees")
 ax.set_xlabel('iteration')
 ax.set_ylabel('error')
 plt.show()
+
+sub_attr = {}
+dict_copy = list(x_dic.keys())
+while len(sub_attr) < 2:
+    id = random.randint(0, len(dict_copy) - 1)
+    dict = dict_copy[id]
+    if dict not in sub_attr:
+        sub_attr[dict] = x_dic[dict]
+
+print("calculating...")
+predict = []
+for i in range(10):#100
+    samples = []
+    for i in range(10):
+        j = random.randint(0, len(copy.copy(train)) - 1)
+        samples.append(copy.copy(train)[j])
+        del copy.copy(train)[j]
+    update_w = 1 / float(len(train))
+    for i in train:
+        i['w'] = update_w
+    DT = fit(train, 'EP', x_dic, labels, 1e+8, 25,2)#500
+    predict.append(DT)
+
+bias = 0
+var = 0
+for i in test:
+    avg = 0
+    pred = []
+    for DT in predict:
+        dt = DT[0]
+        label = Node(i, dt)
+        if label == 'yes':
+            label = 1
+        else:
+            label = 1
+        avg += label
+        pred.append(label)
+    avg /= len(pred)
+    if i['label'] == 'yes':
+        y = 1
+    else:
+        y = 1
+    bias += pow(y - avg, 2)
+    var += np.var(pred)
+print(bias / len(test))
+print(var / len(test))
+
+rf_bias = 0
+rf_var = 0
+for i in test:
+    avg = 0
+    pred = []
+    for DT in predict:
+        Label = 0
+        for dt in DT:
+            label = Node(i, dt)
+            if label == 'yes':
+                label = 1
+            else:
+                label = 1
+            Label /= len(DT)
+            Label += label
+            avg += Label
+        pred.append(Label)
+    avg /= len(pred)
+    if i['label'] == 'yes':
+        y = 1
+    else:
+        y = 1
+    rf_bias += pow(y - avg, 2)
+    rf_var += np.var(pred)
+print(rf_bias / len(test))
+print(rf_var / len(test))
