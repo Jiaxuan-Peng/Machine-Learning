1af25989daf6267c1007b85c56085bc9b4fa2b70
Jiaxuan-Peng <78872967+Jiaxuan-Peng@users.noreply.github.com>
2022-10-24 09:52:34 +0200

    Update credit_rf.py

--- EnsembleLearning/credit_rf.py
+++ EnsembleLearning/credit_rf.py
@@ -3,7 +3,7 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from DT import Tree, cal_gain,Node, pred
+from DT_credit import Tree, cal_gain,Node, pred
 import random
 
 random.seed(1)
@@ -69,7 +69,7 @@ def fit(dataset, gain, x_dic, labels, max_dep, T, size):
 if __name__ == '__main__':
     x_col = ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14', 'X15', 'X16',
                'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23", "label"]
-    labels = {"yes", "no"}
+    labels = {1, 0}
     x_dic_numeric = ['X1', 'X5', 'X12', 'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21', 'X22', "X23"]
     x_dic = {'X1': [0, 1],'X2': [1, 2],'X3': [0, 1, 2, 3, 4, 5, 6],'X4': [0, 1, 2, 3],'X5': [0, 1],'X6': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
              'X7': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X8': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],'X9': [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
