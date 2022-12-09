f82c60423b23d571eb0cb581b7d16be54ad38754
Jiaxuan-Peng <78872967+Jiaxuan-Peng@users.noreply.github.com>
2022-12-08 23:58:39 -0700

    Add files via upload

--- Logistic Regression/ml-h5-lr.py
+++ Logistic Regression/ml-h5-lr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[45]:
+# In[99]:
 
 
 import numpy as np
@@ -10,7 +10,7 @@ from sklearn.utils import shuffle
 from numpy import linalg
 
 
-# In[46]:
+# In[100]:
 
 
 # Set the maximum number of epochs T to 100
@@ -25,17 +25,17 @@ def LR_MAP(x, y, v, gamma, d, T, tolerance):
         x, y = shuffle(x, y,random_state = 1)
         for j in range(len(y)):
             temp = [1/v * i for i in w]
-            L = (-m*y[j]*x.iloc[j]*np.exp(-np.dot(w,x.loc[j])*y[j]))/(1+np.exp(-np.dot(w,x.loc[j])*y[j]))+temp
+            L = (-m*y[j]*x.iloc[j])/(1+np.exp(-np.dot(w,x.loc[j])*y[j]))+temp
             w = w - gamma*L
             gamma = gamma_0/(1+gamma_0/d*i)
-            diff = linalg.norm(L)
+            diff = np.linalg.norm(L)
             #convergence
             if (diff < tolerance):
                 return w
     return w
 
 
-# In[47]:
+# In[101]:
 
 
 # the maximum likelihood (ML) estimation
@@ -48,7 +48,7 @@ def LR_ML(x, y, v, gamma, d, T, tolerance):
     for i in range(T):
         x, y = shuffle(x, y,random_state = 1)
         for j in range(len(y)):
-            L = (-m*y[j]*x.iloc[j]*np.exp(-np.dot(w,x.loc[j])*y[j]))/(1+np.exp(-np.dot(w,x.loc[j])*y[j]))
+            L = (-m*y[j]*x.iloc[j])/(1+np.exp(-np.dot(w,x.loc[j])*y[j]))
             w = w - gamma*L
             gamma = gamma_0/(1+gamma_0/d*i)
             diff = np.linalg.norm(L)
@@ -58,7 +58,7 @@ def LR_ML(x, y, v, gamma, d, T, tolerance):
     return w
 
 
-# In[48]:
+# In[102]:
 
 
 def Prediction_LR(x, y, w):
@@ -71,13 +71,14 @@ def Prediction_LR(x, y, w):
         else:
             y_pred_temp = -1
             y_pred.append(y_pred_temp)
-    error = np.sum(np.abs(y-y_pred))/len(y)
+    error = np.sum(np.abs(y-y_pred))/ 2/len(y)
     return error
 
 
-# In[51]:
+# In[103]:
 
 
+'''
 train = pd.read_csv('train.csv', header=None)
 test = pd.read_csv('test.csv', header=None)
 '''
@@ -92,7 +93,7 @@ with open('/home/u1413911/Downloads/Machine-Learning-main/LinearRegression/concr
     for line in f:
         terms = line.strip().split(',')
         test.append(terms)
-'''        
+        
 train = pd.DataFrame(train,dtype='float64')
 test = pd.DataFrame(test,dtype='float64')
 
@@ -109,7 +110,7 @@ y_test.loc[y_test == 0] = -1
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-# In[ ]:
+# In[106]:
 
 
 variance = [0.01, 0.1, 0.5, 1, 3, 5, 10, 100]
@@ -121,7 +122,7 @@ for v in variance:
     print("variance",v,"error_train:{:.3f}".format(error_train),"error_test:{:.3f}".format(error_test))
 
 
-# In[38]:
+# In[105]:
 
 
 variance = [0.01, 0.1, 0.5, 1, 3, 5, 10, 100]
